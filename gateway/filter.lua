780db8000a3e87775b823bec635d79e8466dd99c
mafrukhin <mafrukhin01@gmail.com>
2026-03-17 17:52:30 +0700

    edit lua

--- gateway/filter.lua
+++ gateway/filter.lua
@@ -1,55 +1,18 @@
-local http = require "resty.http"
-local redis = require "resty.redis"
-
 local args = ngx.req.get_uri_args()
 
 local direct = args["dl"] or os.getenv("DIRECT_LINK_DEFAULT")
 local block = args["bl"] or os.getenv("BLOCK_LINK_DEFAULT")
 
-local ip = ngx.var.remote_addr
 local ua = ngx.var.http_user_agent
 
--- fallback safety
 if not direct then direct = "https://google.com" end
 if not block then block = "https://example.com" end
 
--- BASIC BOT
 if ua == nil then
     return ngx.redirect(block)
 end
 
 if string.find(string.lower(ua), "bot") then
-    return ngx.redirect(block)
-end
-
--- RATE LIMIT (Redis)
-local red = redis:new()
-red:set_timeout(1000)
-
-local ok, err = red:connect("redis", 6379)
-
-if ok then
-    local key = "ip:" .. ip
-    local reqs = red:incr(key)
-
-    if reqs == 1 then
-        red:expire(key, 10)
-    end
-
-    if reqs > 5 then
-        return ngx.redirect(block)
-    end
-end
-
--- CALL BOT ENGINE
-local httpc = http.new()
-
-local res = httpc:request_uri("http://botengine:8080/check", {
-    method = "POST",
-    body = "ip="..ip.."&ua="..(ua or "")
-})
-
-if res and res.body == "block" then
     return ngx.redirect(block)
 end
 
