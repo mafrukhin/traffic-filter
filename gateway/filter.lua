ba184cbfd539521380c6039aab31a263eec7bd52
mafrukhin <mafrukhin01@gmail.com>
2026-05-17 07:10:48 +0700

    fix direct

--- gateway/filter.lua
+++ gateway/filter.lua
@@ -1,4 +1,7 @@
-local args = ngx.req.get_uri_args()
-
--- redirect ke fingerprint dulu
-return ngx.redirect("/fp.html?" .. (ngx.var.query_string or ""))
+-- Only redirect if not already on fp.html
+if ngx.var.uri ~= "/fp.html" then
+  local args = ngx.req.get_uri_args()
+  return ngx.redirect("/fp.html?" .. (ngx.var.query_string or ""))
+end
+-- Let fp.html serve normally
+return nil
